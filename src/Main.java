fa7188cd9c022a659f50743029fe05744a41cc40
Evgeny Rumiantsev <evgenyrumiash@gmail.com>
2022-06-22 22:08:11 +0300

    fix1

--- src/Main.java
+++ src/Main.java
@@ -5,27 +5,24 @@ class Main {
     static int days;
     static int userDays;
 
-
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-
         int x = 0;
-
         while(true){
-            System.out.println("Введите год в формате уууу: ");
+            System.out.println("Введите год в формате уууу и количество дней в этом году: ");
             year = scanner.nextInt();
-            yearMeth();
-
-            System.out.println("Введите количество дней в этом году: ");
             userDays = scanner.nextInt();
+            yearMeth();
             if (days == userDays) {
-                System.out.println("Верно");
                 x++;
             } else {
-                System.out.println("Неверно, в этом году 366 дней");
+                if (days != userDays && userDays == 365){
+                    System.out.println("Неверно, в этом году 366 дней");
+                } else if (days != userDays && userDays == 366) {
+                    System.out.println("Неверно, в этом году 365 дней");
+                }
                 break;
             }
-
         }
         System.out.println("Набрано очков " + x);
     }
